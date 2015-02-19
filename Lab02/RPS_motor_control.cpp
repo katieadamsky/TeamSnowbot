bb755d8bae88fc60da2e7bf58386335999248a76
katieadamsky <katieadamsky@users.noreply.github.com>
2015-02-19 10:40:56 -0500

    Update RPS_motor_control.cpp

--- Lab02/RPS_motor_control.cpp
+++ Lab02/RPS_motor_control.cpp
@@ -28,8 +28,8 @@ void check_x_plus(float x_coordinate) //using RPS while robot is in the +x direc
             left_motor.SetPercent(-75);
             right_motor.SetPercent(-75);
             Sleep(50);
-            left_motor.SetPercent(0);
-            right_motor.SetPercent(0);
+            left_motor.Stop();
+            right_motor.Stop();
             
         }
         else if(RPS.X() < x_coordinate)
@@ -38,11 +38,11 @@ void check_x_plus(float x_coordinate) //using RPS while robot is in the +x direc
             left_motor.SetPercent(75);
             right_motor.SetPercent(75);
             Sleep(50);
-            left_motor.SetPercent(0);
-            right_motor.SetPercent(0);
+            left_motor.Stop();
+            right_motor.Stop();
         }
     }
-}
+}//end check x plus function
 
 void check_y_minus(float y_coordinate) //using RPS while robot is in the -y direction
 {
@@ -55,8 +55,8 @@ void check_y_minus(float y_coordinate) //using RPS while robot is in the -y dire
             left_motor.SetPercent(-75);
             right_motor.SetPercent(-75);
             Sleep(50);
-            left_motor.SetPercent(0);
-            right_motor.SetPercent(0);
+            left_motor.Stop();
+            right_motor.Stop();
         }
         else if(RPS.Y() < y_coordinate)
         {
@@ -64,11 +64,11 @@ void check_y_minus(float y_coordinate) //using RPS while robot is in the -y dire
             left_motor.SetPercent(75);
             right_motor.SetPercent(75);
             Sleep(50);
-            left_motor.SetPercent(0);
-            right_motor.SetPercent(0);
+            left_motor.Stop();
+            right_motor.Stop();
         }
     }
-}
+}//end check y minus
 
 void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direction
 {
@@ -81,8 +81,8 @@ void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direc
             left_motor.SetPercent(75);
             right_motor.SetPercent(75);
             Sleep(50);
-            left_motor.SetPercent(0);
-            right_motor.SetPercent(0);
+            left_motor.Stop();
+            right_motor.Stop();
         }
         else if(RPS.Y() < y_coordinate)
         {
@@ -90,11 +90,11 @@ void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direc
             left_motor.SetPercent(-75);
             right_motor.SetPercent(-75);
             Sleep(50);
-            left_motor.SetPercent(0);
-            right_motor.SetPercent(0);
+            left_motor.Stop();
+            right_motor.Stop();
         }
     }
-}
+}//end check y plus function
 
 void check_heading(float heading) //using RPS
 {
@@ -107,36 +107,36 @@ void check_heading(float heading) //using RPS
         left_motor.SetPercent(75);
         right_motor.SetPercent(-75);
         Sleep(50);
-        left_motor.SetPercent(0);
-        right_motor.SetPercent(0);
+        left_motor.Stop();
+        right_motor.Stop();
     }
     else if ((actual_heading-heading)>-180)
     {
         left_motor.SetPercent(-75);
         right_motor.SetPercent(75);
         Sleep(50);
-        left_motor.SetPercent(0);
-        right_motor.SetPercent(0);
+        left_motor.Stop();
+        right_motor.Stop();
     }
     if ((actual_heading-heading)>180) 
     {
         left_motor.SetPercent(-75);
         right_motor.SetPercent(75);
         Sleep(50);
-        left_motor.SetPercent(0);
-        right_motor.SetPercent(0);
+        left_motor.Stop();
+        right_motor.Stop();
     }
     else if ((actual_heading-heading)<-180)
     {
         left_motor.SetPercent(75);
         right_motor.SetPercent(-75);
         Sleep(50);
-        left_motor.SetPercent(0);
-        right_motor.SetPercent(0);
+        left_motor.Stop();
+        right_motor.Stop();
     }
     Sleep(10);
     }//end while loop
-}
+}//end check heading function
 
 void move_forward(int percent, int counts) //using encoders
 {
@@ -155,21 +155,69 @@ void move_forward(int percent, int counts) //using encoders
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
+}//end move forward function
+
+void turn_left(int percent, int counts) //using encoders
+{
+//Reset encoder counts
+right_encoder.ResetCounts();
+left_encoder.ResetCounts();
+//Set both motors to desired percent
+right_motor.SetPercent(percent);
+left_motor.SetPercent(-percent);
+//While the average of the left and right encoder are less than counts,
+//keep running motors
+        while ((left_encoder.Counts()+right_encoder.Counts())/2<counts)
+        {
+
+        }
+//Turn off motors
+right_motor.Stop();
+left_motor.Stop();
+}//end turn left function
+
+void write_coordinates(void)
+{
+    float x, y, head;
+    x=RPS.X();
+    y=RPS.Y();
+    head=RPS.Heading();
+    LCD.Writeline(x, y, head);
 }
 
 int main(void)
 {
-//drive 14 inches down
-move_forward(75, 297.094);
-//face robot in correct direction
-check_heading(270);//FIGURE OUT WHICH DIRECTION MEANS WHAT
-//change to desired coordinate (18.99,31.000)
-check_y_minus(31.000);
-turn_left(75, 95);
-check_heading(0);
-move_forward(75, 210.094);
-check_x_plus();//DONT FORGET TO ENTER X COORDINATE
-
-
+    LCD.WriteLine("Press the middle button to start");
+while (!buttons.MiddlePressed())
+{
+    //wait for middle button to be pressed
 }
+Sleep(500);
+
+//robot starts at pt A
+write_coordinates();
+check_heading(270.0);  //face robot correct way
+move_forward(75, 297);//drive 14 inches down
+check_y_minus(31.000);//ensure that robot went far enough in y direction
+
+//robot should be at point B
+write_coordinates();//check that it is
+Sleep(500);
+turn_left(75, 95);
+check_heading(0.0);//make sure robot turned a full 90 degrees
+move_forward(75, 211);
+check_x_plus(28.800);
+
+//should be at point C
+write_coordinates();//ensure robot is at point C
+Sleep(500);
+turn_left(75,95);
+check_heading(90.0);//turn robot to face ramp
+move_forward(75,85);//move to point D
+check_y_plus(22.300);//make sure robot is at correct y coordinate
+
+//robot should be at point D
+write_coordinates();//check coordinates for the final time
+Sleep(10000);
+}//end of int main
 
