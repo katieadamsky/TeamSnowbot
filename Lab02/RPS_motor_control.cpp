aaa589b8cf52a3160ce8834cd39d63b0fa740a32
katieadamsky <katieadamsky@users.noreply.github.com>
2015-02-18 20:30:46 -0500

    Update RPS_motor_control.cpp

--- Lab02/RPS_motor_control.cpp
+++ Lab02/RPS_motor_control.cpp
@@ -24,14 +24,18 @@ void check_x_plus(float x_coordinate) //using RPS while robot is in the +x direc
             left_motor.SetPercent(75);
             right_motor.SetPercent(-75);
             Sleep(50);
-            
+            left_motor.SetPercent(0);
+            right_motor.SetPercent(0);
             
         }
         else if(RPS.X() < x_coordinate)
         {
             //pulse the motors for a short duration in the correct direction
-
-            //<ADD CODE HERE>
+            left_motor.SetPercent(-75);
+            right_motor.SetPercent(75);
+            Sleep(50);
+            left_motor.SetPercent(0);
+            right_motor.SetPercent(0);
         }
     }
 }
@@ -44,14 +48,20 @@ void check_y_minus(float y_coordinate) //using RPS while robot is in the -y dire
         if(RPS.Y() > y_coordinate)
         {
             //pulse the motors for a short duration in the correct direction
-
-            //<ADD CODE HERE>
+            left_motor.SetPercent(75);
+            right_motor.SetPercent(75);
+            Sleep(50);
+            left_motor.SetPercent(0);
+            right_motor.SetPercent(0);
         }
         else if(RPS.Y() < y_coordinate)
         {
             //pulse the motors for a short duration in the correct direction
-
-            //<ADD CODE HERE>
+            left_motor.SetPercent(-75);
+            right_motor.SetPercent(-75);
+            Sleep(50);
+            left_motor.SetPercent(0);
+            right_motor.SetPercent(0);
         }
     }
 }
@@ -64,14 +74,20 @@ void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direc
         if(RPS.Y() > y_coordinate)
         {
             //pulse the motors for a short duration in the correct direction
-
-            //<ADD CODE HERE>
+            left_motor.SetPercent(-75);
+            right_motor.SetPercent(-75);
+            Sleep(50);
+            left_motor.SetPercent(0);
+            right_motor.SetPercent(0);
         }
         else if(RPS.Y() < y_coordinate)
         {
             //pulse the motors for a short duration in the correct direction
-
-            //<ADD CODE HERE>
+            left_motor.SetPercent(75);
+            right_motor.SetPercent(75);
+            Sleep(50);
+            left_motor.SetPercent(0);
+            right_motor.SetPercent(0);
         }
     }
 }
@@ -81,10 +97,34 @@ void check_heading(float heading) //using RPS
     //you will need to fill out this one yourself and take into account
     //the edge conditions (when you want the robot to go to 0 degrees
     //or close to 0 degrees)
+    
+}
+
+void move_forward(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder are less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
 }
 
 int main(void)
 {
-
+//drive 14 inches down
+move_forward(75, 297.094);
+//change to desired coordinate (18.99,31.000)
+check_x_plus(18.099);
+check_y_minus(31.000);
 }
 
