71164e776ca5b30b7bb97ad4d7073f279258d02c
katieadamsky <katieadamsky@users.noreply.github.com>
2015-03-03 08:40:13 -0500

    Update V2_00.cpp

--- V2.XX/V2_00.cpp
+++ V2.XX/V2_00.cpp
@@ -9,6 +9,21 @@ RJF 8:00 Team B6*/
 #include <FEHRPS.h>
 #include <FEHServo.h>
 
+#define counts_per_rotation 33.7408
+//define different levels of motor power for easily modifying later
+#define full_power 100
+#define normal_power 75
+#define half_power 50
+#define order1 1
+#define order2 2
+#define order3 3
+#define order4 4
+#define order5 5
+#define order6 6
+#define topbutton 7
+#define middlebutton 8
+#define bottombutton 9
+
 //Declarations for encoders & motors
 ButtonBoard buttons(FEHIO::Bank3);
 DigitalEncoder right_encoder(FEHIO::P0_0);
@@ -17,3 +32,8 @@ FEHMotor right_motor(FEHMotor::Motor0);
 FEHMotor left_motor(FEHMotor::Motor1);
 AnalogInputPin cds(FEHIO::P0_4); //CdS cell
 FEHServo servo(FEHServo::Servo0);
+
+void press_buttons(void){
+  //WHAT IS THE FUNCTION TO INTERPRET RPS DATA
+  
+}
