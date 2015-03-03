60e488bf48bfd7424c6950940bd6376acef35bf2
katieadamsky <katieadamsky@users.noreply.github.com>
2015-03-03 08:20:03 -0500

    Create V2_00.cpp

---
+++ V2.XX/V2_00.cpp
@@ -0,0 +1,19 @@
+/*Performance Test 3: Pressing the buttons
+Written by Katie Adamsky 2/18/15
+RJF 8:00 Team B6*/
+
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHRPS.h>
+#include <FEHServo.h>
+
+//Declarations for encoders & motors
+ButtonBoard buttons(FEHIO::Bank3);
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_2);
+FEHMotor right_motor(FEHMotor::Motor0);
+FEHMotor left_motor(FEHMotor::Motor1);
+AnalogInputPin cds(FEHIO::P0_4); //CdS cell
+FEHServo servo(FEHServo::Servo0);
