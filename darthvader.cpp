f4bbffcaa6a08a51ef927d63adca2d1ecb04959c
katieadamsky <katieadamsky@users.noreply.github.com>
2015-03-25 11:11:42 -0400

    Update darthvader.cpp

--- darthvader.cpp
+++ darthvader.cpp
@@ -8,5 +8,100 @@
 
 int main (void)
 {
-
+  Buzzer.Tone(392);
+  Sleep(500);
+  Buzzer.Tone(392);
+  Sleep(500);
+  Buzzer.Tone(392);
+  Sleep(500);
+  Buzzer.Tone(311);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  
+  Buzzer.Tone(392);
+  Sleep(500);
+  Buzzer.Tone(311);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  Buzzer.Tone(392);
+  Sleep(1000);
+  
+  Buzzer.Tone(587);
+  Sleep(500);
+  Buzzer.Tone(587);
+  Sleep(500);
+  Buzzer.Tone(587);
+  Sleep(500);
+  Buzzer.Tone(622);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  
+  Buzzer.Tone(370);
+  Sleep(500);
+  Buzzer.Tone(311);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  Buzzer.Tone(370);
+  Sleep(1000);
+  
+  Buzzer.Tone(784);
+  Sleep(500);
+  Buzzer.Tone(392);
+  Sleep(375);
+  Buzzer.Tone(392);
+  Sleep(375);
+  Buzzer.Tone(784);
+  Sleep(500);
+  Buzzer.Tone(740);
+  Sleep(375);
+  Buzzer.Tone(698);
+  Sleep(125);
+  
+  Buzzer.Tone(659);
+  Sleep(125);
+  Buzzer.Tone(622);
+  Sleep(125);
+  Buzzer.Tone(659);
+  Sleep(250);
+  Buzzer.Tone(139);
+  Sleep(250);
+  
+  Buzzer.Tone(415);
+  Sleep(250);
+  Buzzer.Tone(554);
+  Sleep(500);
+  Buzzer.Tone(523);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  
+  Buzzer.Tone(466);
+  Sleep(125);
+  Buzzer.Tone(440);
+  Sleep(125);
+  Buzzer.Tone(466);
+  Sleep(250);
+  Buzzer.Tone(156);
+  Sleep(250);
+  Buzzer.Tone(311);
+  Sleep(250);
+  Buzzer.Tone(370);
+  Sleep(500);
+  Buzzer.Tone(311);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  
+  Buzzer.Tone(392);
+  Sleep(500);
+  Buzzer.Tone(311);
+  Sleep(375);
+  Buzzer.Tone(466);
+  Sleep(125);
+  Buzzer.Tone(392);
+  Sleep(1000);
 }
